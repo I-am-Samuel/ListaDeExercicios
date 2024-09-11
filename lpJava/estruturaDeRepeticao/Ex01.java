14fea115d23dfbc3ae0b1eb02a150ad17d28c572
Samuel Eduardo <samueleduardorocha@gmail.com>
2024-09-11 10:25:31 -0300

    Adicionado estrutura de Repetição (Ex01)

---
+++ lpJava/estruturaDeRepeticao/Ex01.java
@@ -0,0 +1,28 @@
+package lpJava.estruturaDeRepeticao;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa que peça uma nota, entre zero e dez. 
+ * Mostre uma mensagem caso o valor seja inválido e 
+ * continue pedindo até que o usuário informe um valor válido.
+ */
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int valor;
+
+        while (true) {
+            System.out.println("Digite um valor de 0 a 10");
+            valor = scanner.nextInt();
+
+            if (valor < 0 || valor > 10) {
+                System.out.println("Erro: digite um valor de 0 a 10!");
+            } else{
+                break;
+            }
+        }
+    }
+    
+}
